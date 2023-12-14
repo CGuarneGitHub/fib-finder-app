b7fcccc57066969533100edbfc833ee8385096b4
CGuarneGitHub <christopher.guarnera@lightwellinc.com>
2023-12-14 15:37:58 -0500

    updating styling, error handling, moving from localhost

--- site/fib.js
+++ site/fib.js
@@ -14,10 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function onSearchFib() {
 
+    displaySpinner();
+
+    const request = createRequest();
+
     const inStartingNum = document.getElementById("inStartingNumber");
-   
+    const requestBody = JSON.stringify({
+        startingNumber: inStartingNum.value
+    })
+
+    request.send(requestBody);
+
+}
+
+function createRequest() {
     const request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:8080/findFib");
+    request.open("POST", "http://3.15.229.114/findFib");
     request.setRequestHeader("Content-Type", "application/json");
     request.responseType = "json";
     
@@ -26,24 +38,60 @@ function onSearchFib() {
         if(request.readyState === 4 
             && request.status === 200) {
 
-                const result = request.response;
+                onSuccess(request.response);
                 
-                const spnNextFibNum = document.getElementById("spnNextFibNum");
-                spnNextFibNum.innerText = result.nextFibNum;
-
-                const h2 = document.querySelector("h2");
-                h2.classList.remove("hidden");
-                
-
-
+        } else if (request.readyState === 4
+            && request.status !== 200) {
+                hideSpinner();
+                displayError("An error has occurred. Please try again.")
             }
 
     }
 
-    const requestBody = JSON.stringify({
-        startingNumber: inStartingNum.value
-    })
+    request.onerror = () => {
+        hideSpinner();
+        displayError("An error has occurred. Please try again.")
+    }
 
-    request.send(requestBody);
+    request.ontimeout = () => {
+        hideSpinner();
+        displayError("Request has timed out. Please try again.")
+    }
 
+    return request;
+}
+
+function displaySpinner() {
+    const spinner = document.getElementById("spinner");
+    spinner.classList.remove("hidden");
+}
+
+function hideSpinner() {
+    const spinner = document.getElementById("spinner");
+    spinner.classList.add("hidden");
+}
+
+
+function clearError(){
+    const errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerText = "";
+    errorMsg.classList.add("hidden");
+}
+
+function displayError(msg) {
+    const errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerText = msg;
+    errorMsg.classList.remove("hidden");
+
+}
+
+function onSuccess(result){
+
+    const divNextFibNum = document.getElementById("divNextFibNum");
+    divNextFibNum.innerText = result.nextFibNum;
+
+    hideSpinner();
+
+    const h2 = document.querySelector("h2");
+    h2.classList.remove("hidden");
 }
